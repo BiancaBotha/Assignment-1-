011d3bb63a28b2de9cecb7ebf5ef1ae6101c461b
unknown <User@INFBN-56.up.ac.za>
2019-02-07 16:29:34 +0200

    Initial commit

---
+++ Site.js
@@ -0,0 +1,5 @@
+<script>
+FunctionKalonga(){
+   window.alert(I am Excited for INF 354 Because we will be developing our Programming abilities and become certified Professionals in our field, Provided we work hard.);
+}
+</script>
